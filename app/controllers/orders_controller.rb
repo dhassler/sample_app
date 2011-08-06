cae145e1d554c1e63e42dafdff4f2413286e221a
David Hassler <dhassler@gmail.com>
2011-08-06 09:04:19 -0600

    Updated order summaries on order pages

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,22 +1,36 @@
 class OrdersController < ApplicationController
+
+  require 'yaml'
+  
   def new
     @order_items = params[:order_items]
-    @order_items_formatted = Hash.new
-    @order_items.each do |item_id, qty|
-      @order_items_formatted[MenuItem.find(item_id)] = qty if qty.to_i > 0
-    end
+    @order_items.delete_if{ |key,value| value.to_i <= 0 }
+    @order_items_formatted = create_formatted_order_items(@order_items)
     @order = Order.new(:order_items => @order_items)
     render 'delivery'
   end
 
   def save
     @order = Order.new(params[:order])
+    @order.order_items = eval(@order.order_items)
+    @order_items_formatted = create_formatted_order_items(@order.order_items)
     if @order.save
       render 'complete'
     else
-      flash[:error] = "Please supply name and phone"
+      flash.now[:error] = "Please supply name and phone"
       render 'delivery'
     end
   end
+  
+  private
+
+  def create_formatted_order_items(order_items)
+    order_items_formatted = Hash.new
+    order_items.each do |item_id, qty|
+      order_items_formatted[MenuItem.find(item_id)] = qty.to_i
+    end
+    order_items_formatted
+  end
+  
 end
 
